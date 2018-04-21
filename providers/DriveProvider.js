ff2106c9a1558dda1bd19d6ed421a13fe1e90420
Ahmad Arif <ahmad.arif019@gmail.com>
2018-04-22 00:13:38 +0700

    change provider name

---
+++ providers/DriveProvider.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { ServiceProvider } = require('@adonisjs/fold')
+const Minio = require('./src/Drivers')
+
+class DriveProvider extends ServiceProvider {
+  register () {
+    this.app.extend('Adonis/Addons/Drive', 'minio', () => {
+      return Minio
+    })
+  }
+}
+
+module.exports = DriveProvider
